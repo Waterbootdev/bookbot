9144129cf931838c11c21e07f71fc06ef3b37949
Waterbootdev <wassermann.sp@gmail.com>
2025-04-26 11:50:47 +0200

    Lesson CountCaracters

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_characters
 
 def get_book_text(path_to_book):
 
     with open(path_to_book) as file:
         return file.read()
 
-def main():
-    path_to_book = "books/frankenstein.txt"
-    book_text = get_book_text(path_to_book)
-    number_of_words = get_num_words(book_text)
-    message = f"{number_of_words} words found in the document"
+def print_count_message(count, type):
+
+    message = f"{count} {type} found in the document"
+
     print(message)
 
+    
+
+def main():
+
+    #read the book
+    path_to_book = "books/frankenstein.txt"
+    book_text = get_book_text(path_to_book)
+
+    #count inside the book
+    number_of_words = get_num_words(book_text)
+    number_of_chars = get_num_characters(book_text)
+
+    #print counts
+    print_count_message(number_of_words, "words")
+    print(number_of_chars)
+
+    
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,15 @@ def get_num_words(book_text):
 
     words = book_text.split()
     return len(words)
+
+def get_num_characters(book_text):
+    
+    lower_book_text = book_text.lower()
+    sorted_keys = sorted(set(lower_book_text))
+    dict_num_characters = {key:0 for key in sorted_keys}
+
+    for char in lower_book_text:
+        dict_num_characters[char] += 1
+
+    return dict_num_characters
+
