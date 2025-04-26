8573572663d2e19b302426fd7a6e3f0cfa9678a7
Waterbootdev <wassermann.sp@gmail.com>
2025-04-26 13:41:05 +0200

    Lesson Print a Report refactored III

--- prints.py
+++ prints.py
@@ -1,19 +1,19 @@
 from stats import char_key, num_key
 
-def print_char_counts(char_counts_list):
+def print_char_counts(printable_char_counts):
 
     print("--------- Character Count -------")
     
-    for char_count in char_counts_list:
+    for char_count in printable_char_counts:
         char = char_count[char_key]
         num = char_count[num_key]
         print(f"{char}: {num}")
 
-def print_word_count(count):
+def print_word_count(word_count):
 
     print("----------- Word Count ----------")
 
-    print(f"Found {count} total words")
+    print(f"Found {word_count} total words")
 
 def print_bookbot_start():
     print("============ BOOKBOT ============")
@@ -24,11 +24,11 @@ def print_bookbot_end():
 def print_source(path_to_book):
     print(f"Analyzing book found at {path_to_book}...")
 
-def print_bookbot(path_to_book, word_count, char_counts_list):
+def print_bookbot(path_to_book, word_count, printable_char_counts):
     print_bookbot_start()
     print_source(path_to_book)
     print_word_count(word_count)
-    print_char_counts(char_counts_list)
+    print_char_counts(printable_char_counts)
     print_bookbot_end()
 
     
